06851a702587212650a3608ae1e34f42b621f552
Visnu Pitiyanuvath <visnup@gmail.com>
2016-03-04 23:02:12 -0800

    a board!

--- board.jsx
+++ board.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import Card from './card'
+import css from './board.css'
 
 class Board extends React.Component {
   render() {
-    return <div>{this.props.words.map(word => <Card word={word} />)}</div>
+    return (
+      <div className={css.className}>
+        {this.props.words.map(word => {
+          return <Card key={String(word.id)} word={word.word} />
+        })}
+      </div>
+    )
   }
 }
 
--- index.jsx
+++ index.jsx
@@ -9,7 +9,7 @@ class App extends React.Component {
 
   componentDidMount() {
     fetch('http://api.wordnik.com/v4/words.json/randomWords?includePartOfSpeech=noun&minCorpusCount=1000000&minLength=4&limit=25&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5').then(res => res.json()).then((json) => {
-      this.setState({ words: json.map(word => word.word) })
+      this.setState({ words: json })
     });
   }
 
