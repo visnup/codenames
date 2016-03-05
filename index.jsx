0fe608380ccadd9b0340858fda42f1bc19065f54
Visnu Pitiyanuvath <visnup@gmail.com>
2016-03-05 00:05:43 -0800

    more words

--- index.jsx
+++ index.jsx
@@ -6,21 +6,23 @@ import Board from './board'
 
 class App extends React.Component {
   state = {
-    words: ['initial']
+    words: []
   }
 
   componentDidMount() {
     const params = qs.stringify({
-      includePartOfSpeech: 'noun,adjective,verb',
-      minCorpusCount: 1000000,
-      minDictionaryCount: 20,
-      minLength: 4,
+      includePartOfSpeech: 'noun,verb',
+      minCorpusCount: 100000,
+      minDictionaryCount: 5,
+      minLength: 3,
+      maxLength: 10,
       limit: 25,
       api_key: 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
     })
     fetch(`http://api.wordnik.com/v4/words.json/randomWords?${params}`)
       .then(res => res.json())
       .then((json) => {
+        console.log(json.length)
         this.setState({ words: shuffle(json) })
       })
   }
